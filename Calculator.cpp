ccb8def0adef473f0f1a92d32afdb0a4f8df821c
Andrew <121867332+WinsalotNot@users.noreply.github.com>
2023-10-06 21:13:12 +0700

    Added Parsing String

---
+++ Calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Calculator.h"
+#include <string>
+using namespace std;
+
+void ParsingString(string UserInput, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        cout << UserInput[i] << endl;
+    }
+}
+
+int main()
+{
+    string Input;
+    cout << "Enter string: ";
+    getline(cin, Input);
+    ParsingString(Input, Input.size());
+}
---
+++ Calculator.h
@@ -0,0 +1,9 @@
+#ifndef SCI_CALC
+#define SCI_CALC
+using namespace std;
+
+void ParsingString(string UserInput, int count);
+
+
+
+#endif
