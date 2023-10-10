158a56d75fe9ef9322da62988cc99fc605b74698
Shifinn <63943150+Shifinn@users.noreply.github.com>
2023-10-10 17:05:43 +0700

    Update Index.cpp

--- Index.cpp
+++ Index.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include<string>
+#include "Sci_Calcu.h"
+using namespace std;
 
+index::index(): equation(""), result(0) {}
+index::index(string& n, float r) : equation(n), result(r) {}
+void index::print(){
+    cout << "\tEquation: " << equation << endl;
+        cout << "\tThe result: " << result << endl<<endl;
+}
+
+ // Function to insert data to a previously initialized class
+ void index::Insert(string& n, float r){
+    equation = n;
+    result = r;
+ }
